ac8b2240b233fd2be80310beb56872ee801d526a
flyingbucket <2311067@mail.nankai.edu.cn>
2026-03-23 10:54:25 +0800

    feat(graph): update GraphOps

--- include/graph/GraphOps.h
+++ include/graph/GraphOps.h
@@ -7,6 +7,13 @@ extern "C" {
 #endif
 
 typedef int VertexId;
+typedef double Weight;
+
+typedef struct GraphInfoOps {
+  int (*vertex_count)(void* G);
+  bool (*valid_vertex)(void* G, VertexId v);
+  bool (*directed)(void* G);
+} GraphInfoOps;
 
 typedef struct GraphQueryOps {
   bool (*adjacent)(void* G, VertexId v1, VertexId v2);
@@ -17,24 +24,9 @@ typedef struct GraphQueryOps {
 typedef struct GraphMutateOps {
   VertexId (*add_vert)(void* G);
   bool (*delete_vert)(void* G, VertexId v);
-  bool (*add_edge)(void* G, VertexId v1, VertexId v2);
+  bool (*add_edge)(void* G, VertexId v1, VertexId v2, Weight w);
   bool (*remove_edge)(void* G, VertexId v1, VertexId v2);
 } GraphMutateOps;
-
-typedef enum {
-  WEIGHT_INT,
-  WEIGHT_DOUBLE,
-  WEIGHT_FLOAT,
-} WeightType;
-
-typedef struct {
-  WeightType type;
-  union {
-    int i;
-    double d;
-    float f;
-  } value;
-} Weight;
 
 typedef struct WeightedGraphOps {
   Weight (*update_edge_weight)(void* G, VertexId v1, VertexId v2, Weight w);
@@ -42,6 +34,7 @@ typedef struct WeightedGraphOps {
 } WeightedGraphOps;
 
 typedef struct {
+  const GraphInfoOps* iops;
   const GraphQueryOps* qops;
   const GraphMutateOps* mops;
   const WeightedGraphOps* wops;
@@ -51,4 +44,18 @@ typedef struct {
 }
 #endif
 
+// typedef enum {
+//   WEIGHT_INT,
+//   WEIGHT_DOUBLE,
+//   WEIGHT_FLOAT,
+// } WeightType;
+//
+// typedef struct {
+//   WeightType type;
+//   union {
+//     int i;
+//     double d;
+//     float f;
+//   } value;
+// } Weight;
 #endif  // !INCLUDE_GRAPH_GRAPAOPS_H
